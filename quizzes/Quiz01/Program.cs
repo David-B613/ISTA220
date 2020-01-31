18c5306c06acd6057cc08b6c816717003d596b1f
David-B613 <60449471+David-B613@users.noreply.github.com>
2020-01-31 14:28:43 -0500

    ISTA220 first upload

---
+++ quizzes/Quiz01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Part 1");
+
+            Console.WriteLine("MM          MM          SSSSSSSS                  SSSSSSSS              AAAAAA");
+            Console.WriteLine("MMM         MMM         SSSSSSSS                  SSSSSSSS              AA AA");
+            Console.WriteLine("MMMM       MMMM         SS                       SS                   AA      AA");
+            Console.WriteLine("MMMM       MMMM         SS                       SS                   AA      AA"); 
+            Console.WriteLine("MM   MMMM    MM         SSSSSSSS                   SSSSSSSS           AAAAAAAAA");
+            Console.WriteLine("MM    MM     MM         SSSSSSSS                   SSSSSSSS           AAAAAAAAAA");
+            Console.WriteLine("MM           MM                SS                        SS          AA        AA");
+            Console.WriteLine("MM           MM                SS                        SS          AA        AA");
+            Console.WriteLine("MM           MM          SSSSSSSS                  SSSSSSSS          AA        AA");
+            Console.WriteLine("MM           MM          SSSSSSSS                  SSSSSSSS          AA        AA");
+
+            Console.WriteLine("Part 2");
+            double phi = (1 + Math.Sqrt (5)) / 2;
+            Console.WriteLine(phi);
+
+
+
+
+        }
+    }
+}
